06d80e8192f4bc8ce345c8ad000f5114c7f8c017
thpeng <thierry.peng@gmail.com>
2019-11-18 12:13:56 +0100

    enhanced solution for expired groups

--- src/main/java/ch/thp/proto/IntegrationAggregateApplication.java
+++ src/main/java/ch/thp/proto/IntegrationAggregateApplication.java
@@ -1,8 +1,10 @@
 package ch.thp.proto;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.integration.aggregator.DefaultAggregatingMessageGroupProcessor;
 import org.springframework.integration.aggregator.TimeoutCountSequenceSizeReleaseStrategy;
 import org.springframework.integration.channel.PublishSubscribeChannel;
@@ -13,7 +15,16 @@ import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.integration.store.MessageGroupStore;
+import org.springframework.integration.store.MessageGroupStoreReaper;
+import org.springframework.integration.store.SimpleMessageStore;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -24,6 +35,8 @@ import java.util.concurrent.TimeUnit;
 @SpringBootApplication
 @EnableIntegration
 @EnableAsync
+@Slf4j
+@EnableScheduling
 public class IntegrationAggregateApplication {
 
 	public static void main(String[] args) {
@@ -55,6 +68,22 @@ public class IntegrationAggregateApplication {
 				.channel(inChannel())
 				.get();
 	}
+	@Bean
+	public MessageGroupStore messageGroupStore(){
+		return new SimpleMessageStore();
+	}
+
+	@Bean
+	public MessageGroupStoreReaper messageGroupStoreReaper(){
+		MessageGroupStoreReaper messageGroupStoreReaper = new MessageGroupStoreReaper(messageGroupStore());
+		messageGroupStoreReaper.setTimeout(2000);
+		return messageGroupStoreReaper;
+	}
+	@Scheduled(fixedDelay = 3000)
+	public void executeReaper(){
+		log.info("executing reaper");
+		messageGroupStoreReaper().run();
+	}
 
 	@Bean
 	public PublishSubscribeChannel inChannel(){
@@ -69,12 +98,36 @@ public class IntegrationAggregateApplication {
 						.correlationStrategy(m -> ((Datatype) m.getPayload()).getGroup())
 				.releaseStrategy(new TimeoutCountSequenceSizeReleaseStrategy(100, 2000))
 				.requiresReply(false)
-				.expireGroupsUponCompletion(true))
+				.expireGroupsUponCompletion(true)
+				.messageStore(messageGroupStore())
+				.taskScheduler(aggregatorScheduler()))
+				.channel(decoupling())
 				.<List<Datatype>, Set<Datatype>>transform(t -> new HashSet<>(t))
 				.split()
 				.handle(new LoggingHandler("INFO"))
 				.get();
 
+	}
+
+	@Bean
+	public MessageChannel decoupling(){
+		return MessageChannels.executor("decoupling", taskExecutor()).get();
+	}
+
+	@Bean
+	public TaskExecutor taskExecutor(){
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		threadPoolTaskExecutor.setCorePoolSize(5);
+		threadPoolTaskExecutor.setMaxPoolSize(50);
+		threadPoolTaskExecutor.setThreadNamePrefix("whatever2-");
+		return threadPoolTaskExecutor;
+	}
+	@Bean
+	public TaskScheduler aggregatorScheduler(){
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		threadPoolTaskScheduler.setPoolSize(10);
+		threadPoolTaskScheduler.setThreadNamePrefix("whatever-");
+		return threadPoolTaskScheduler;
 	}
 	@Bean
 	public DefaultAggregatingMessageGroupProcessor processor(){
--- src/main/java/ch/thp/proto/Source.java
+++ src/main/java/ch/thp/proto/Source.java
@@ -7,6 +7,7 @@ import java.util.Random;
 
 @Slf4j
 public class Source {
+    int i = 0;
 
     private String name;
 
@@ -17,11 +18,21 @@ public class Source {
     Random random = new Random();
 
     public Datatype from(){
+        if(i>10){
+            return null;
+        }
+        String group = null;
+        if(name.equals("B") && i<2){
+            group = 3+"";
+        } else {
+            group = String.valueOf(random.nextInt(2));
+        }
 
-        Datatype datatype = new Datatype(String.valueOf(random.nextInt(2)),
+        Datatype datatype = new Datatype(group,
                 String.valueOf(random.nextInt(2)),
                 name);
         log.info("generated: {}", datatype);
+        i++;
         return datatype;
     }
 }
